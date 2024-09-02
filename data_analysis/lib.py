c7dc18aa31e3c65689d5f9b5ff532fe1bb09fb92
Maria Kesa <maria.kesa@gmail.com>
2024-09-02 23:33:17 +0300

    plotting

--- data_analysis/lib.py
+++ data_analysis/lib.py
@@ -59,8 +59,12 @@ def plot_cells(experiment_container_id, experiment_type, stimulus_type):
     ix2cell={ix:cell for ix, cell in enumerate(cells)}
     stim_table=data_set_regression.get_stimulus_table(stimulus_type)
     stim_dict=create_nested_dict(stim_table)
-    timed_array=get_cell_trials_f(get_n_events, 0, data_set_events,stim_dict)
-    W, H = get_cell_nmf(timed_array)
+    ix=cell2ix[575003602]
+    cell_ix=ix2cell[ix]
+    timed_array=get_cell_trials_f(get_n_events, ix, data_set_events,stim_dict)
+    coninuous_array=get_cell_trials_regression(get_n_events, cell_ix, data_set_regression, stim_dict)
+    #W, H = get_cell_nmf(timed_array)
+    plot_timed_array(coninuous_array)
 
 def plot_components(H):
         # Plot the H matrix (temporal components) in 3D
@@ -79,14 +83,47 @@ def plot_components(H):
     
     plt.show()
 
+def plot_timed_array(timed_array):
+    n_trials = timed_array.shape[0]
+    n_timepoints = timed_array.shape[1]
+
+    # Set up the figure with a height large enough to fit 10 subplots
+    fig, axes = plt.subplots(n_trials, 1, figsize=(10, 2 * n_trials), sharex=True)
+    
+    for i in range(n_trials):
+        axes[i].plot(timed_array[i], label=f'Trial {i+1}')
+        axes[i].legend(loc='upper right')
+        axes[i].set_ylabel('Event Value')
+        axes[i].grid(True)
+    
+    # Set the label for the x-axis for the last subplot
+    axes[-1].set_xlabel('Time Points')
+    
+    plt.tight_layout()
+    plt.show()
+
 def get_cell_nmf(timed_array):
     # Perform non-negative matrix factorization
     model = NMF(n_components=3, init='random', random_state=0)
     W = model.fit_transform(timed_array)
     H = model.components_
     print(H.shape,W.shape)
-    plot_components(H)
+    plt.plot(H[2])
+    plt.show()
+    #plot_components(H)
     return W, H
+
+def get_cell_trials_regression(action_function, cell_ix, regression_data, stim_dict):
+    all_trials = regression_data.get_fluorescence_traces(cell_specimen_ids=[cell_ix])[1][0]
+    print(all_trials)
+    start_times = np.array([stim_dict[j]['start'] for j in stim_dict])
+    end_times = np.array([stim_dict[j]['end'] for j in stim_dict])
+
+    # Vectorized calculation of max_pool over the specified ranges
+    timed_array = np.array([[max_pool(all_trials, start, end) for start, end in zip(starts, ends)] 
+                            for starts, ends in zip(start_times, end_times)])
+
+    return timed_array
 
 
 def get_cell_trials_f(action_function, cell_ix, events, stim_dict):
@@ -106,3 +143,4 @@ def cell_compute_n_events(experiment_id, cell_specimen_id):
 
 #natural_movie_three: 10 repeats
 plot_cells(experiment_container_id, 'three_session_A', 'natural_movie_three')
+plot_cells(experiment_container_id, 'three_session_C', 'natural_movie_one')
