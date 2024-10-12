12c90dda20276260ce9ba2f0f55c302584161909
Maria Kesa <maria.kesa@gmail.com>
2024-10-13 02:38:56 +0300

    variation across dimensions

--- pipeline/lib_clean.py
+++ pipeline/lib_clean.py
@@ -51,7 +51,7 @@ class FrontierPipeline:
         embeddings=pickle.load(open('/home/maria/Documents/HuggingMouseData/TransformerEmbeddings/google_vit-base-patch16-224-in21k_embeddings.pkl','rb'))['natural_movie_one']
         self.embeddings=torch.tensor(embeddings, dtype=torch.float32)
 
-    def training_loop(self, lnp_model, real_spikes_tensor):
+    def training_loop(self, lnp_model, real_spikes_tensor, trial_index):
         # Training loop
         # Loss function (Negative Log-Likelihood) and Optimizer
         criterion = nn.PoissonNLLLoss(log_input=False)  # Poisson Negative Log-Likelihood Loss
@@ -78,20 +78,21 @@ class FrontierPipeline:
         print('Training finished')
         print(lnp_model.linear.weight.shape)
         weights=lnp_model.linear.weight.detach().numpy()
-        np.save('weights.npy',weights)  
+        np.save(f'./weights_to_analyze/weights_{trial_index}.npy',weights)  
 
-    def __call__(self, stimulus_type='natural_movie_one_more_repeats'):
+    def __call__(self, trial_index, stimulus_type='natural_movie_one_more_repeats'):
         stim = self.stimuli_df[self.stimuli_df['stimulus_name'] == stimulus_type]
         spike_times=self.session.spike_times
         spike_times=get_spike_intervals(spike_times,stim['start_time'].values,stim['stop_time'].values)
-        spikes=torch.tensor([spike_times[key] for key in spike_times.keys()],dtype=torch.float32)[:,:900].T
+        spikes=torch.tensor([spike_times[key] for key in spike_times.keys()],dtype=torch.float32)[:,trial_index*900:(trial_index+1)*900].T
         print(spikes.shape)
         lnp=LNPModel(self.embeddings.shape[1], len(spike_times.keys()))
         print(self.embeddings.shape)   
         print(lnp(self.embeddings).shape)
-        self.training_loop(lnp,spikes)
+        self.training_loop(lnp,spikes,trial_index)
 
 pipeline=FrontierPipeline()
-pipeline()
+for i in range(60):
+    pipeline(i)
 
         
