7fffd5727c7f6184a3b1ceee7a0d71989f0b876c
Maria Kesa <maria.kesa@gmail.com>
2024-10-19 20:10:45 +0300

    rastermap functional

---
+++ sequences_rastermap/lib.py
@@ -0,0 +1,56 @@
+import os
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+from dotenv import load_dotenv
+import numpy as np
+
+load_dotenv()
+
+stimulus_repeats_dict={'natural_movie_one_more_repeats': 60}
+stimulus_length_dict={'natural_movie_one_more_repeats': 900}    
+
+def get_spike_intervals(spike_times, start_times, stop_times):
+    spikes_in_intervals = {}
+
+    # Convert start_times and stop_times to numpy arrays for faster operations
+    start_times = np.array(start_times)
+    stop_times = np.array(stop_times)
+
+    # Loop through each neuron, but optimize the spike finding with vectorized operations
+    for neuron_id, times in spike_times.items():
+        # Convert times to a numpy array if it's not already
+        times = np.array(times)
+        
+        # Use numpy's searchsorted to find the indices where the start and stop times would fit
+        start_indices = np.searchsorted(times, start_times, side='left')
+        stop_indices = np.searchsorted(times, stop_times, side='right')
+        
+        # Get the number of spikes in each interval by subtracting indices
+        spikes_in_intervals[neuron_id] = stop_indices - start_indices
+    
+    return spikes_in_intervals
+
+def get_session(session_id):
+    data_dir=os.environ['ALLEN_NEUROPIXELS_PATH']
+    manifest_path = os.path.join(data_dir, "manifest.json")
+    cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+    session= cache.get_session_data(session_id)
+    return session
+
+def get_context(stimulus_type, session):
+    stimuli_df=session.stimulus_presentations
+    stim = stimuli_df[stimuli_df['stimulus_name'] == stimulus_type]
+    spike_times = session.spike_times
+    return stim, spike_times
+
+def get_spikes(spike_times, stim, trial_index, stimulus_type):
+    spike_times=get_spike_intervals(spike_times,stim['start_time'].values,stim['stop_time'].values)
+    spikes=np.array([spike_times[key] for key in spike_times.keys()])[:,trial_index*stimulus_length_dict[stimulus_type]:(trial_index+1)*stimulus_length_dict[stimulus_type]].T
+    return spikes
+
+def pipeline(stimulus_type='natural_movie_one_more_repeats', session_id=831882777):
+    session=get_session(session_id)
+    stim, spike_times = get_context(stimulus_type, session)
+    n_trials=stimulus_repeats_dict[stimulus_type]
+    for trial in range(n_trials):
+        spikes=get_spikes(spike_times, stim, trial, stimulus_type)
+    
