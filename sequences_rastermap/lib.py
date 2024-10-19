160a85e16009e5e97d06d90bd301dc926ed3298d
Maria Kesa <maria.kesa@gmail.com>
2024-10-19 20:44:32 +0300

    algorithm development

--- sequences_rastermap/lib.py
+++ sequences_rastermap/lib.py
@@ -44,13 +44,30 @@ def get_context(stimulus_type, session):
 
 def get_spikes(spike_times, stim, trial_index, stimulus_type):
     spike_times=get_spike_intervals(spike_times,stim['start_time'].values,stim['stop_time'].values)
-    spikes=np.array([spike_times[key] for key in spike_times.keys()])[:,trial_index*stimulus_length_dict[stimulus_type]:(trial_index+1)*stimulus_length_dict[stimulus_type]].T
-    return spikes
+    spikes=np.array([spike_times[key] for key in spike_times.keys()])[:,trial_index*stimulus_length_dict[stimulus_type]:(trial_index+1)*stimulus_length_dict[stimulus_type]]
+    all_zero_neurons=np.where(np.sum(spikes, axis=1)==0)[0]
+    return spikes, all_zero_neurons
 
-def pipeline(stimulus_type='natural_movie_one_more_repeats', session_id=831882777):
+
+def rastermap_pipeline(stimulus_type='natural_movie_one_more_repeats', session_id=831882777):
     session=get_session(session_id)
     stim, spike_times = get_context(stimulus_type, session)
     n_trials=stimulus_repeats_dict[stimulus_type]
+    #n_trials=1
+    n_trials=5
+    all_zero_neuron_dct={}
+    spikes_dct={}
     for trial in range(n_trials):
-        spikes=get_spikes(spike_times, stim, trial, stimulus_type)
+        spikes, all_zero_neurons=get_spikes(spike_times, stim, trial, stimulus_type)
+        all_zero_neuron_dct[trial]=all_zero_neurons
+        spikes_dct[trial]=spikes
+    for trial_1 in range(n_trials):
+        for trial_2 in range(n_trials):
+            if trial_1!=trial_2:
+                neurons_to_exclude=list(set(all_zero_neuron_dct[trial_1]).union(set(all_zero_neuron_dct[trial_2])))
+                
+                
+    
+
+rastermap_pipeline()
     
